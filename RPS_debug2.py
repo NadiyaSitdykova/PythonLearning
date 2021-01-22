7bbcba0021bda205f4e5f2627525a7d3971a5ae8
Nadiya Sitdykova <rovenskasa@gmail.com>
2021-01-21 19:14:15 -0800

    Update RPS_debug2.py

--- RPS_debug2.py
+++ RPS_debug2.py
@@ -3,33 +3,32 @@
 """Rock Paper Scissors game
 This game will allow the user to play rock, paper, scissors with the computer.
 """
-
-import randint from random 
+from random import randint
 
 options = ["ROCK", "PAPER", "SCISSORS"]
 
-message: ["tie" = "Yawn it's a tie",
+message = {"tie": "Yawn it's a tie",
            "won": "Yay you won!",
-           "lost" = "Aww you lost!"] 
+           "lost": "Aww you lost!"} 
 
-decide_winner(user_choice): 
+def decide_winner(user_choice, computer_choice): 
     print "you selected: %s" % user_choice
-    print "computer selected: %d" % computer_choice 
+    print "computer selected: %s" % computer_choice 
 
     if user_choice == computer_choice:
-        print message["lost"]
+        print message["tie"]
     elif user_choice == options[0] and computer_choice == options[2]: 
         print message["won"]
     elif user_choice == options[1] and computer_choice == options[0]:
         print message["won"]
-    if user_choice == options[3] and computer_choice == options[2]:
+    elif user_choice == options[2] and computer_choice == options[1]:
         print message["won"]
     else:
-        print message["tie"]
+        print message["lost"]
       
 def play_RPS():
-    raw_input == user_choice("Enter Rock, Paper, or Scissors")
-    user_choice.upper()
-    computer_choice = options[randint(2,0)]
+    user_choice = raw_input("Enter Rock, Paper, or Scissors")
+    user_choice = user_choice.upper()
+    computer_choice = options[randint(0,2)]
     decide_winner(user_choice, computer_choice)
-    play_RPS()
+play_RPS()
