304bc372d6fc1424b914e63d27a49d850c60840c
Nadiya Sitdykova <rovenskasa@gmail.com>
2021-01-21 19:07:00 -0800

    Update RPS_debug.py

--- RPS_debug.py
+++ RPS_debug.py
@@ -3,33 +3,45 @@
 """Rock Paper Scissors game
 This game will allow the user to play rock, paper, scissors with the computer.
 """
-
-import randint from random 
+# SyntaxError: invalid syntax (incorrect order of importing)
+from random import randint 
 
 options = ["ROCK", "PAPER", "SCISSORS"]
 
-message: ["tie" = "Yawn it's a tie",
+# SyntaxError: invalid syntax; dictionaries require curly brackets rather than hard brackets on both ends
+# Also SemanticError: there should be an equal sign instead of a colon after message; a colon used to annotate variables
+message = {"tie": "Yawn it's a tie",
            "won": "Yay you won!",
-           "lost" = "Aww you lost!"] 
+           "lost": "Aww you lost!"} 
 
-decide_winner(user_choice): 
+# SyntaxError: invalid syntax (function not properly defined; computer_choice not passed as argument)
+def decide_winner(user_choice, computer_choice):
     print "you selected: %s" % user_choice
-    print "computer selected: %d" % computer_choice 
+    # TypeError: %d format: a number is required, not str
+    print "computer selected: %s" % computer_choice 
 
     if user_choice == computer_choice:
-        print message["lost"]
+        # Semantic error: "lost" should read "tie"; code will run but will not produce the intended results)
+        print message["tie"]
     elif user_choice == options[0] and computer_choice == options[2]: 
         print message["won"]
     elif user_choice == options[1] and computer_choice == options[0]:
         print message["won"]
-    if user_choice == options[3] and computer_choice == options[2]:
+    # Semantic error: 'if' used instead of 'elif' (code will run but will not produce the intended results)
+    # IndexError: list index out of range (list indices begin at 0 and not 1)
+    elif user_choice == options[2] and computer_choice == options[1]:
         print message["won"]
     else:
-        print message["tie"]
+        # Semantic error: "tie" should read "lost"; code will run but will not produce the intended results)
+        print message["lost"]
       
 def play_RPS():
-    raw_input == user_choice("Enter Rock, Paper, or Scissors")
-    user_choice.upper()
-    computer_choice = options[randint(2,0)]
+    # raw_input and user_choice are swapped; equality used instead of assignment
+    user_choice = raw_input("Enter Rock, Paper, or Scissors")
+    # Semantic error: user_choice.upper() needs to be saved in user_choice (code will run but will not produce the intended results)       
+    user_choice = user_choice.upper()
+    # ValueError: empty range for randrange() (2,1,-1)
+    computer_choice = options[randint(0,2)]
     decide_winner(user_choice, computer_choice)
-    play_RPS()
+# Semantic error: play_RPS() should not be indented (code will run but will not play the game)
+play_RPS()
