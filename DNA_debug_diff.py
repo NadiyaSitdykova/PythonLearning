318022e1566dc734c6d4c1b76c764c0adcd2877b
Nadiya Sitdykova <rovenskasa@gmail.com>
2021-02-16 15:54:25 -0800

    Update DNA_debug_diff.py

--- DNA_debug_diff.py
+++ DNA_debug_diff.py
@@ -1,32 +1,32 @@
 sample = ['GTA','GGG','CAC']
 
-def read_dna():
-  dna_data = []
-  with open(dna_file, 'w') as f:
-    for line in file:
+def read_dna(dna_file):
+  dna_data = ""
+  with open(dna_file, 'r') as f:
+    for line in f:
       dna_data += line
   return dna_data
 
 def dna_codons(dna):
-  codons = {}
-  for i in range(0, 3, len(dna)):
-    if (i += 3) < len(dna):
-      codons.append(dna[i])
+  codons = []
+  for i in range(0, len(dna), 3):
+    if (i + 3) < len(dna):
+      codons.append(dna[i:(i+3)])
   return codons
 
 def match_dna(dna):
-  matches = "0"
+  matches = 0
   for codon in dna:
-    if codon is in sample:
-      matches += "1"
-    return matches
+    if codon in sample:
+      matches += 1
+  return matches
 
 def is_criminal(dna_sample):
   dna_data = read_dna(dna_sample)
   codons = dna_codons(dna_data)
   num_matches = match_dna(codons)
-  if num_matches >= 3
-    print "%f matches found. Continue investigation." % num_matches
+  if num_matches >= 3:
+    print "%d matches found. Continue investigation." % num_matches
   else:
     print "%d matches found. Free suspect." % num_matches
 
